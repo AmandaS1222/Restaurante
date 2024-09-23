af9ffd7737bc438b4438914d576883971b5defd2
AmandaSantos <contatoflorencecity@gmail.com>
2024-09-23 20:01:26 +0000

    Salvando dados

--- Menu restaurante.py
+++ Menu restaurante.py
@@ -1,27 +1,33 @@
 print("""
     Menu:
-    1 - Strogonoff
-    2 - Feijoada
-    3 - Lasanha
-    4 - Moqueca
-    5 - Pizza
-    6 - Churrasco
-    7 - Sushi
+    1 - Strogonoff - 15
+    2 - Feijoada - 10
+    3 - Lasanha - 18
+    4 - Moqueca - 25
+    5 - Pizza - 30
+    6 - Churrasco - 19
+    7 - Sushi -20
 """)
+
+armazenamento = []
 
 while True:
     prato = int(input("Digite o código do prato desejado: "))
-    
-    if prato < 1:
-        print("Código inválido, escreve um código válido")
-        break
-    else:
-        prato > 7
-        print("Código inválido, escreve um código válido")
+    armazenamento.append(prato)
 
-    int(input("Deseja adicionar outro pedido? "))
-    Pedido_extra = int(input("Digite o código do prato que deseja: "))
+    if prato < 1 or prato > 7:
+        print("Código inválido")
+        break
+
+    resposta = float(input("Deseja adicionar outro pedido? "))
     
+    if resposta == "sim":
+        Pedido_extra = int(input("Digite o código do prato que deseja: "))
+        armazenamento.append(0)
+    else:
+        resposta == "não"
+        break
+        
     match(Pedido_extra):
         case "1":
             nome = "Strogonoff"
@@ -44,3 +50,6 @@ while True:
         case "7":
             nome = "Sushi"
             codigo = 7
+
+    for prato in armazenamento:
+        print(f"Valores: {prato}")
