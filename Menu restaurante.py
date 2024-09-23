507e1e22f150bbf1e93dbc9d9fd839e6e36a74b1
AmandaSantos <contatoflorencecity@gmail.com>
2024-09-23 19:46:41 +0000

    Restaurante2

--- Menu restaurante.py
+++ Menu restaurante.py
@@ -10,12 +10,37 @@ print("""
 """)
 
 while True:
-    codigo = int(input("Digite o código do prato desejado: "))
+    prato = int(input("Digite o código do prato desejado: "))
     
-    if codigo < 1:
-        print("Código inválido")
+    if prato < 1:
+        print("Código inválido, escreve um código válido")
         break
     else:
-        codigo > 7
-        print("Código inválido")
+        prato > 7
+        print("Código inválido, escreve um código válido")
 
+    int(input("Deseja adicionar outro pedido? "))
+    Pedido_extra = int(input("Digite o código do prato que deseja: "))
+    
+    match(Pedido_extra):
+        case "1":
+            nome = "Strogonoff"
+            codigo = 1 
+        case "2":
+            nome = "Feijoada"
+            codigo = 2
+        case "3":
+            nome = "Lasanha"
+            codigo = 3
+        case "4":
+            nome = "Moqueca"
+            codigo = 4
+        case "5":
+            nome = "Pizza"
+            codigo = 5
+        case "6":
+            nome = "Churrasco"
+            codigo = 6
+        case "7":
+            nome = "Sushi"
+            codigo = 7
