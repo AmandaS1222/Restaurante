bee01028920efc27dba8ac953957faffd0bc984f
gabe07s <GABRIELNVS05@GMAIL.COM>
2024-09-23 20:01:37 +0000

    gabriel.1

---
+++ Sistema de desconto, pagamento e add.py
@@ -0,0 +1,31 @@
+# valorbruto = 0
+#def resultado_final(desc_apli)
+      print(f""" VALORES
+
+ """)
+#
+#
+#
+# valorbruto = valorbruto + soma 
+# pagamento = input(f"Qual seria a forma de pagamento? Caso for á vista, digite 'V'. Caso for credito, digite 'D' ")
+# if pagamento == "V"  
+#   valorreal = valorbruto - (valorbruto / 10)
+#   desc_apli == "-"   
+# elif pagamento == "C"
+#   valorreal = valorbruto + (valorbruto / 10)
+#   des_apli == "+"
+#
+#
+#
+#
+#
+#print(""" VALORES """)
+#
+#
+#
+#
+#
+#
+#
+#
+#
