da18cc89a5ab966d062f84f9e07e9250a66af3f0
gabe07s <GABRIELNVS05@GMAIL.COM>
2024-09-25 16:32:24 +0000

    Salvo 02

--- Sistema de desconto, pagamento e add.py
+++ Sistema de desconto, pagamento e add.py
@@ -1,32 +1,25 @@
-# valorbruto = 0
-#def resultado_final(desc_apli)
-      
-print(f""" VALORES
+import os
+os.system("cls || clear")
 
- """)
-#
-#
-#
-# valorbruto = valorbruto + soma 
-# pagamento = input(f"Qual seria a forma de pagamento? Caso for á vista, digite 'V'. Caso for credito, digite 'D' ")
-# if pagamento == "V"  
-#   valorreal = valorbruto - (valorbruto / 10)
-#   desc_apli == "-"   
-# elif pagamento == "C"
-#   valorreal = valorbruto + (valorbruto / 10)
-#   des_apli == "+"
-#
-#
-#
-#
-#
-#print(""" VALORES """)
-#
-#
-#
-#
-#
-#
-#
-#
-#
+valorbruto = 0
+def resultado_final(desc_apli, nome):
+    print('-'*27)
+    print(f"""========VALOR FINAL========
+          {desc_apli} e {nome}""")
+
+valorbruto = 14
+soma = 6
+valorbruto = valorbruto + soma 
+while True:
+    pagamento = input(f"Qual seria a forma de pagamento? Caso for á vista, digite 'V'. Caso for credito, digite 'C' ")
+    if pagamento == "V":
+        valorreal = valorbruto - (valorbruto / 10)
+        resultado_final("-","Desconto:")
+        break
+    elif pagamento == "C":
+        valorreal = valorbruto + (valorbruto / 10)
+        resultado_final("+","Taxa:")
+        break
+    else:
+        print("CODIGO INCORRETO, UTILIZE 'V' E 'C'")
+resultado_final()
